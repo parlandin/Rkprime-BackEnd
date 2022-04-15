e8e732ba3cd11a9bfca372375e279ec85ecd5124
gustavo parlandim <gustavo.miguel.dias.parlandim@gmail.com>
2022-04-15 20:21:29 -0300

    creating product controller and routes

--- src/routes/products.route.ts
+++ src/routes/products.route.ts
@@ -3,13 +3,18 @@ import ProductsController from "../controllers/products.controller";
 
 const products = Router();
 
-products.post("/produtos", ProductsController.newProduct);
-
 products.get("/produtos", ProductsController.showProducts);
-products.get("/produtos/:id", ProductsController.showProducts);
-products.get("produtos/categorias", ProductsController.showCategorys);
-products.get("/produtos/categoria/:Namecategoria", ProductsController.showProductToCategory);
+products.get("/produtos/categorias", ProductsController.showCategorys);
+products.get("/produtos/categoria/:Namecategoria", ProductsController.showProductByCategory);
 products.get("/produtos/destaques", ProductsController.showProductSpotlight);
+products.get("/produtos/:id", ProductsController.showProductById);
+
+
+//
+products.post("/produtos", ProductsController.newProduct);
+products.put("/produtos/:id", ProductsController.updateProduct);
+//products.patch("/produtos/:id", ProductsController.newProduct);
+products.delete("/produtos/:id", ProductsController.deleteProduct);
 
 
 export default products;
