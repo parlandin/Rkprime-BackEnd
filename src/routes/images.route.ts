aba4848b6eb26e247667a180fb660210fea025fa
gustavo parlandim <gustavo.miguel.dias.parlandim@gmail.com>
2022-04-19 20:36:21 -0300

    feature, save local image

---
+++ src/routes/images.route.ts
@@ -0,0 +1,10 @@
+/* import {Router, static} from "express";
+const path = require('path')
+
+
+const routeImages = Router();
+
+routeImages.use((express.static(__dirname + '/public')))
+
+
+export default routeImages; */
