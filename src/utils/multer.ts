a26bd7adabcae88edee783ab389dae0696e3b858
gustavo parlandim <gustavo.miguel.dias.parlandim@gmail.com>
2022-04-22 21:58:49 -0300

    add google fireStore sdk

--- src/utils/multer.ts
+++ src/utils/multer.ts
@@ -5,13 +5,15 @@ import crypto from "crypto";
 
 
 export default multer({
-  storage: multer.diskStorage({
+    //storage: multer.memoryStorage(),
+ /*  storage: multer.diskStorage({
     filename(request, file, callback){
-       
         const hash = crypto.randomBytes(5).toString("hex");
         const filename = `${hash}-${file.originalname}`
-
         callback(null, filename);
+    },
+  }), */
+  limits: {
+      fileSize: 1024 * 1024
   }
-  })
 });
