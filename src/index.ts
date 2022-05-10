f89d2b4e55cbde25c6342c58a77e5759179e718f
gustavo parlandim <gustavo.miguel.dias.parlandim@gmail.com>
2022-05-10 20:22:33 -0300

    config middlewares

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import Session from "./routes/sessions.route";
 import MongoConnection from "./database/MongoConnection";
 import cors from 'cors';
 import path from "path";
+import helmet from "helmet";
+import morgan from "morgan";
 
 
 const app = express();
@@ -14,9 +16,14 @@ const port = process.env.PORT || 5000;
 
 
 //Configurações do servidor
+app.use(helmet());
+app.use(morgan('combined'))
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cors());
+app.use(cors({
+    origin: 'http://rk-prime.vercel.app/'
+}));
+
 
 
 //conectado com o bando de dados
