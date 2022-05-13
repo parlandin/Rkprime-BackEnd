d1c6eacd6b06c32d9d7f9a95064aba6a83fe386b
gustavo parlandim <gustavo.miguel.dias.parlandim@gmail.com>
2022-05-12 22:16:31 -0300

    headers

--- src/middleware/siteAutorization.ts
+++ src/middleware/siteAutorization.ts
@@ -3,6 +3,6 @@ import {Request, Response, NextFunction} from "express"
 
 export default function siteAutorization(req: Request, res: Response, next: NextFunction){
     const permission = req.headers.referer == "https://rk-prime.vercel.app" ? true : false;
-    if(!permission) return res.status(401).json({message: "authorization invalid"});
-    return next();
+    //if(!permission) return res.status(401).json({message: "authorization invalid"});
+    return res.status(200).json({header: req.headers.referer});
 }
