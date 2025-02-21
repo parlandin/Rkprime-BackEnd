b0533cec31aae68b9cdd02a4bd59f384f08d09c3
parlandin <56051040+parlandin@users.noreply.github.com>
2025-02-21 14:46:33 -0300

    refactor: improve product controller methods for better readability and error handling

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,64 +1,55 @@
 import { NextFunction, Request, Response } from "express";
 import productsDB from "../database/models/product.model";
-import {deleteFile} from "../services/drive"
-
+import { deleteFile } from "../services/drive";
 
 class ProductsController {
+    public async newProduct(req: Request, res: Response) {
+        if (!req.file) return res.status(404).json({ message: "arquivo não enviado" });
 
-    public async newProduct(req: Request, res: Response){ 
-        if(!req.file) return res.status(404).json({message: "arquivo não enviado"})
-
-        const image = req.file.filename
-        const dateRequest = JSON.parse(req.body.data)
+        const image = req.file.filename;
+        const dateRequest = JSON.parse(req.body.data);
 
         const img = [
             `http://localhost:5000/images/${image}`,
             "http://localhost:5000/images/402c204efe-meramente ilustrativo.jpg",
             "http://localhost:5000/images/02d24881c1-meramente ilustrativo2.jpg",
             "http://localhost:5000/images/6c97e1ef27-Sem Título-1.jpg"
-        ]
-           
+        ];
 
         try {
-            
-
-            const {nome, descricao, preco, categoria, destaque, tags, quantidade, nota} = dateRequest;
-            const newProduct = await productsDB.create({nome, descricao, preco, imagens: img, categoria, destaque, tags, quantidade, nota});
-            res.status(201).json({message: "produto cadastrado com sucesso"});
-            return;
-
+            const { nome, descricao, preco, categoria, destaque, tags, quantidade, nota } = dateRequest;
+            const newProduct = await productsDB.create({ 
+                nome, descricao, preco, imagens: img, 
+                categoria, destaque, tags, quantidade, nota 
+            });
+            res.status(201).json({ message: "produto cadastrado com sucesso" });
         } catch (error) {
             console.log(error);
             res.sendStatus(404);
         }
-        
     }
 
-    public async showProducts(req: Request, res: Response){
+    public async showProducts(req: Request, res: Response) {
         try {
-            const products = await productsDB.find({em_breve: false}, '_id nome descricao preco imagens categoria quantidade tags destaque nota');
+            const products = await productsDB.find(
+                {  }
+            ).select('_id nome descricao preco imagens categoria quantidade tags destaque nota');
+            
             res.status(200).json(products);
-            return;
-
-        }catch (error) {
+        } catch (error) {
             console.log(error);
             res.sendStatus(404);
-            return;
         }
-       
     }
 
-    public async showProductById(req: Request, res: Response){
+    public async showProductById(req: Request, res: Response) {
         try {
             const { id } = req.params;
-            const products = await productsDB.findOne({_id: id}); 
-            res.status(200).json(products);
-            return;
-
+            const product = await productsDB.findById(id);
+            res.status(200).json(product);
         } catch (error) {
             console.log(error);
             res.sendStatus(500);
-            return;
         }
     }
 
@@ -118,42 +109,37 @@ class ProductsController {
        
     }
 
-    public async updateProduct(req: Request, res: Response){
+    public async updateProduct(req: Request, res: Response) {
         try {
             const { id } = req.params;
-            const {nome, descricao, preco, imagens, categoria, destaque, tags , quantidade} = req.body;
-            await productsDB.updateOne({_id: id}, {$set: {nome, descricao, preco, imagens, categoria, destaque, tags, quantidade}}); 
+            const { nome, descricao, preco, imagens, categoria, destaque, tags, quantidade } = req.body;
+            
+            await productsDB.findByIdAndUpdate(id, {
+                nome, descricao, preco, imagens, 
+                categoria, destaque, tags, quantidade
+            });
+            
             res.sendStatus(200);
-            return;
-
         } catch (error) {
             console.log(error);
             res.sendStatus(500);
-            return;
         }
     }
 
     public async deleteProduct(req: Request, res: Response) {
         const { id } = req.params;
         try {
+            const product = await productsDB.findById(id).select('imagens');
 
-            const {imagens} = await productsDB.findOne({_id: id}, 'imagens');
+            if (product?.imagens?.length > 0) {
+                await deleteFile(product.imagens[0]);
+            }
 
-
-            if(imagens !== null || imagens.length > 0 ){
-                //for(let file of imagens){
-                    await deleteFile(imagens[0]);
-                    
-                }
-           
-            await productsDB.deleteOne({_id: id});
+            await productsDB.findByIdAndDelete(id);
             res.sendStatus(200);
-            return;
-
         } catch (error) {
             console.log(error);
             res.sendStatus(500);
-            return;
         }
     }
 }
