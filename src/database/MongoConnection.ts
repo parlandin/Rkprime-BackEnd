5fc463fee5bdb09b5fb8c17a4cafe291d59781da
parlandin <56051040+parlandin@users.noreply.github.com>
2025-02-21 14:46:57 -0300

    fix: update MongoDB connection to use the correct host configuration

--- src/database/MongoConnection.ts
+++ src/database/MongoConnection.ts
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
 import config from "../config/database"
+import producs from "./models/product.model"
 
 class MongoConnection {
     public async connect(): Promise<void> {
         try {
-            await mongoose.connect(config.hostlocal);
-            console.log("database conectdado");
+            await mongoose.connect(config.host);
+            console.log("database conectdado");    
+
+           
             
         }catch(error){
             console.error(error);
